438e5bc116efcd228799aec36531a0df42895339
liuzhihu1993 <18851170455@139.com>
2017-06-15 15:47:05 +0800

    添加 jdbc的realm验证

--- src/com/github/liuzhihu/shiro_1/LoginLogoutTest.java
+++ src/com/github/liuzhihu/shiro_1/LoginLogoutTest.java
@@ -7,6 +7,8 @@ import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
+import org.apache.shiro.util.ThreadContext;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -85,7 +87,7 @@ public class LoginLogoutTest
     public void testCustomMultiRealm()
     {
         //1.第一步：获取SecurityManager工厂类
-        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-realm.ini");
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-multi-realm.ini");
         
         //2.第二步：从共厂中获取SecurityManager
         SecurityManager securityManager = factory.getInstance();
@@ -112,6 +114,48 @@ public class LoginLogoutTest
         
         //6、退出
         subject.logout();
+    }
+    
+    //测试jdbc
+    @Test
+    public void testJdbcRealm()
+    {
+        //1.第一步：获取SecurityManager工厂类
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-jdbc-realm.ini");
+        
+        //2.第二步：从共厂中获取SecurityManager
+        SecurityManager securityManager = factory.getInstance();
+        
+        //3.第三步：将得到的securityManager实例，绑定给SecurityUtils
+        SecurityUtils.setSecurityManager(securityManager);
+        
+        //4.第四步：得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("liu", "123");
+        
+        try
+        {
+            //5.第五步：登录
+            subject.login(token);
+        }
+        catch (AuthenticationException e)
+        {
+            System.out.println("AuthenticationException e:" + e);
+        }
+        
+        //6.添加断言      
+        Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
+        
+        //6、退出
+        subject.logout();
+    }
+    
+    @After
+    public void tearDown()
+        throws Exception
+    {
+        //退出时请解除绑定Subject到线程 否则对下次测试造成影响
+        ThreadContext.unbindSubject();
     }
     
 }
