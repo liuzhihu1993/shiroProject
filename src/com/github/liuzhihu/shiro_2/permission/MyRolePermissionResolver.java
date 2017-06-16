a8aad4cba926be3052f5d2e03e3e23920b8c46e5
liuzhihu1993 <18851170455@139.com>
2017-06-16 13:01:28 +0800

    增加Authorizer、PermissionResolver及RolePermissionResolver校验相关代码

---
+++ src/com/github/liuzhihu/shiro_2/permission/MyRolePermissionResolver.java
@@ -0,0 +1,21 @@
+package com.github.liuzhihu.shiro_2.permission;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.apache.shiro.authz.Permission;
+import org.apache.shiro.authz.permission.RolePermissionResolver;
+import org.apache.shiro.authz.permission.WildcardPermission;
+
+public class MyRolePermissionResolver implements RolePermissionResolver
+{
+    @Override
+    public Collection<Permission> resolvePermissionsInRole(String roleString)
+    {
+        if ("role1".equals(roleString))
+        {
+            return Arrays.asList((Permission)new WildcardPermission("menu:*"));
+        }
+        return null;
+    }
+}
